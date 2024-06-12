a99e134dda1a297e1f9d95f6e72f7d07118d9bdf
Bekjan <temenovbekzan@gmail.com>
2024-06-12 12:40:09 +0600

    header

--- src/Components/Header/Advertising.jsx
+++ src/Components/Header/Advertising.jsx
@@ -1,10 +1,10 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import reklama from "../../assets/bekjan/svg/reklama.svg"
-import icon from "../../assets/bekjan/svg/icon1.svg"
-// import icon1 from "../../assets/bekjan/svg/icon2.svg"
-// import icon2 from "../../assets/bekjan/svg/icon3.svg"
-// import icon3 from "../../assets/bekjan/svg/icon4.svg"
-// import icon4 from "../../assets/bekjan/svg/icon5.svg"
+import reklama from "../../assets/bekjan/svg/reklama.svg";
+import icon from "../../assets/bekjan/svg/icon1.svg";
+import icon1 from "../../assets/bekjan/svg/icon2.svg";
+import icon2 from "../../assets/bekjan/svg/icon3.svg";
+import icon3 from "../../assets/bekjan/svg/icon4.svg";
+import icon4 from "../../assets/bekjan/svg/icon5.svg";
 
 import 'swiper/css';
 import 'swiper/css/pagination';
@@ -12,32 +12,42 @@ import 'swiper/css/navigation';
 
 import './Header.css';
 
-import { Pagination, Navigation } from 'swiper/modules';
+import { Autoplay } from 'swiper/modules';
 
-export default function App() {
-    return (
-        <>
-            <Swiper
-                slidesPerView={1}
-                spaceBetween={30}
-                loop={true}
-                navigation={true}
-                modules={[Pagination, Navigation]}
-                className="mySwiper"
-            >
-                <SwiperSlide>
-                    <div className='reclama'>
-                        <img className='rec-img' src={reklama} alt="img"/>
-                        <div className='rec-texts'>
-                            <img className='rec-icon' src={icon} alt="icon"/>
-                            <h1 className='rec-text'>ДОСТАВКА В ДЕНЬ ЗАКАЗА</h1>
-                        </div>
+const slidesData = [
+    { img: reklama, icon: icon, text: "ДОСТАВКА В ДЕНЬ ЗАКАЗА" },
+    { img: reklama, icon: icon1, text: "ГАРАНТИЯ КАЧЕСТВА" },
+    { img: reklama, icon: icon2, text: "САМАЯ НИЗКАЯ ЦЕНА" },
+    { img: reklama, icon: icon3, text: "ШИРОКИЙ АССОРТИМЕНТ" },
+    { img: reklama, icon: icon4, text: "АКЦИИ И СКИДКИ" },
+];
+
+const App = () => (
+    <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        style={{ "--swiper-navigation-size": "15px" }}
+        autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+        }}
+        modules={[Autoplay]}
+
+        className="mySwiper"
+    >
+        {slidesData.map((slide, index) => (
+            <SwiperSlide key={index}>
+                <div className='reclama'>
+                    <img className='rec-img' src={slide.img} alt="reklama" />
+                    <div className='rec-texts'>
+                        <img className='rec-icon' src={slide.icon} alt="icon" />
+                        <h1 className='rec-text'>{slide.text}</h1>
                     </div>
-                </SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-            </Swiper>
-        </>
-    );
-}
+                </div>
+            </SwiperSlide>
+        ))}
+    </Swiper>
+);
+
+export default App;
