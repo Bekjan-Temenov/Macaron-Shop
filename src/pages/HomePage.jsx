5d26d03b498162112223dab46c945e0661240acc
Suna <minsuna63@gmail.com>
2024-06-18 08:19:41 -0400

    sunitos

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,13 @@
 import Home from "../Components/Home/Home";
+import BasketPage from "./Basket/BasketPage";
+import Deliv from "./Deliv/Deliv";
 
 function HomePage() {
   return (
     <div>
       <Home/>
+      <BasketPage/>
+      <Deliv/>
     </div>
   );
 }
